4addf92afadef18e7c51d0588397a10ec5723743
pwischh <166182277+pwischh@users.noreply.github.com>
2024-06-25 17:03:34 -0400

    Add files via upload

--- navbarButtons.js
+++ navbarButtons.js
@@ -4,10 +4,13 @@ navbarButtons.forEach(button => {
     button.addEventListener("click", function(){
         let openPanel = document.getElementsByClassName("panel-open");
         openPanel[0].classList.toggle("panel-open");
+
         let openVis = document.getElementsByClassName("vis-open");
         openVis[0].classList.toggle("vis-open");
+
         let panel = document.getElementsByClassName(this.dataset.panel);
         panel[0].classList.toggle("panel-open");
+
         let vis = document.getElementsByClassName(this.dataset.vis);
         vis[0].classList.toggle("vis-open");
     })
